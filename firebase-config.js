77dce96c1dee831eef22fd64d1593b320c989271
thecompanyunltd <ulilaptop@gmail.com>
2025-06-26 22:06:04 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Replace with your actual Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAUDoAdsxET7YN6uCtVpibRe8WiWMOEgKM",
+  authDomain: "gayafterdark-7756c.firebaseapp.com",
+  projectId: "gayafterdark-7756c",
+  storageBucket: "gayafterdark-7756c.firebasestorage.app",
+  messagingSenderId: "786967220854",
+  appId: "1:786967220854:web:90eef1d553a26dcb2e8557",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
